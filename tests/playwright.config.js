93806e1d73ba372778cb3970afdc919a30fc446e
Pooja Marne <marnepooja1520@gmail.com>
2026-07-10 23:20:29 +0530

    Only 1 browser

--- tests/playwright.config.js
+++ tests/playwright.config.js
@@ -33,18 +33,6 @@ module.exports = defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-    {
-      name: 'mobile-chrome',
-      use: { ...devices['Pixel 5'] },
-    },
+    }
   ],
 });
