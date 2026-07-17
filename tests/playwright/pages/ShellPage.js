0bab0908b9e1d7f2736e09b0a3587724e76d7de3
Pooja Walhekar <poojama@incubxperts.com>
2026-07-17 14:12:32 +0530

    teste

--- tests/playwright/pages/ShellPage.js
+++ tests/playwright/pages/ShellPage.js
@@ -6,9 +6,7 @@ class ShellPage {
     this.page = page;
     this.navMenu = page.locator(resolve('ShellPage.navMenu', '[data-test="nav-menu"]'));
     this.navDashboard = page.locator(resolve('ShellPage.navDashboard', '[data-test="nav-dashboard"]'));
-    this.navProducts = page.locator(resolve('ShellPage.navProducts', '[data-test="nav-products"]'));
     this.navUsers = page.locator(resolve('ShellPage.navUsers', '[data-test="nav-users"]'));
-    this.navOrders = page.locator(resolve('ShellPage.navOrders', '[data-test="nav-orders"]'));
     this.logoutButton = page.locator(resolve('ShellPage.logoutButton', '[data-test="logout-button"]'));
     this.loggedInUser = page.locator(resolve('ShellPage.loggedInUser', '[data-test="logged-in-user"]'));
   }
