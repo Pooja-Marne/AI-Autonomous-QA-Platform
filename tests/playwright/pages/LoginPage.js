7781184fb66f1c22eb2ac7af9f416d3f2ba71f68
Pooja Walhekar <poojama@incubxperts.com>
2026-07-17 15:16:50 +0530

    pede

--- tests/playwright/pages/LoginPage.js
+++ tests/playwright/pages/LoginPage.js
@@ -4,7 +4,7 @@ class LoginPage {
   constructor(page) {
     this.page = page;
     this.usernameInput = page.locator(resolve('LoginPage.usernameInput', '[data-test="uname"]'));
-    this.passwordInput = page.locator(resolve('LoginPage.passwordInput', '[data-test="password"]'));
+    this.passwordInput = page.locator(resolve('LoginPage.passwordInput', '[data-test="pass"]'));
     this.loginButton = page.locator(resolve('LoginPage.loginButton', '[data-test="login"]'));
     this.errorText = page.locator(resolve('LoginPage.errorText', '[data-test="login-error"]'));
   }
